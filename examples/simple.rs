b1f37c63fd79ffd3975e2056192c0958cf871482
Ethan Frei <ethan.frei@gmail.com>
2014-09-15 19:39:38 +0000

    added a validation rule for using locations in gameweekdays.  changed some struct names to be more terse.

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,11 +1,17 @@
 extern crate schedule_gen;
 
+use schedule_gen::{ Date, GameWeekday, IdAndName, GameTime, Time };
+
 fn main() {
     println!("Hello world!");
 
-    /*let thing = schedule_gen::LeagueSpec {
+    let thing = schedule_gen::LeagueSpec {
         teams: vec![],
-        locations: vec![],
+        locations: vec![
+            IdAndName { id: "1".to_string(), name: "name1".to_string() },
+            IdAndName { id: "2".to_string(), name: "name2".to_string() },
+            IdAndName { id: "3".to_string(), name: "name3".to_string() }
+        ],
         start_date: Date {
             day: 15,
             month: 9,
@@ -16,11 +22,35 @@ fn main() {
             month: 9,
             year: 2014
         },
-        day_and_times: vec![
-            DayAndTimes {
+        game_weekdays: vec![
+            GameWeekday {
                 day: schedule_gen::Monday,
-                time_and_locations: vec![]
+                game_times: vec![
+                    GameTime {
+                        time: Time {
+                            hour: 16,
+                            min: 0
+                        },
+                        location_ids: vec!["1".to_string(), "2".to_string()]
+                    },
+                    GameTime {
+                        time: Time {
+                            hour: 17,
+                            min: 0
+                        },
+                        location_ids: vec!["1".to_string(), "3".to_string()]
+                    },
+                    GameTime {
+                        time: Time {
+                            hour: 18,
+                            min: 0
+                        },
+                        location_ids: vec!["2".to_string(), "3".to_string()]
+                    }
+                ]
             }
         ]
-    };*/
+    };
+
+    schedule_gen::validate(thing);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,40 +5,41 @@ extern crate chrono;
 use chrono::{ NaiveDate, Datelike };
 use chrono::Weekday as ChronoWeekday;
 
+use std::collections::HashSet;
 
 pub struct LeagueSpec {
-    teams: Vec<IdAndName>,
-    locations: Vec<IdAndName>,
-    start_date: Date,
-    end_date: Date,
-    day_and_times: Vec<DayAndTimes>
+    pub teams: Vec<IdAndName>,
+    pub locations: Vec<IdAndName>,
+    pub start_date: Date,
+    pub end_date: Date,
+    pub game_weekdays: Vec<GameWeekday>
 }
 
 
 pub struct IdAndName {
-    id: String,
-    name: String
+    pub id: String,
+    pub name: String
 }
 
 pub struct Time {
-    hour: u8,
-    min: u8
+    pub hour: u8,
+    pub min: u8
 }
 
 pub struct Date {
-    day: u8,
-    month: u8,
-    year: u16
+    pub day: u8,
+    pub month: u8,
+    pub year: u16
 }
 
-pub struct DayAndTimes {
-    day: Weekday,
-    time_and_locations: Vec<TimeAndLocationIds>
+pub struct GameWeekday {
+    pub day: Weekday,
+    pub game_times: Vec<GameTime>
 }
 
-pub struct TimeAndLocationIds {
-    time: Time,
-    location_ids: Vec<String>
+pub struct GameTime {
+    pub time: Time,
+    pub location_ids: Vec<String>
 }
 
 pub enum Weekday {
@@ -66,7 +67,15 @@ impl Weekday {
     }
 
     pub fn from_chrono_weekday(chrono_weekday: ChronoWeekday) -> Weekday {
-        Sunday
+        match chrono_weekday {
+            chrono::Sun => Sunday,
+            chrono::Mon => Monday,
+            chrono::Tue => Tuesday,
+            chrono::Wed => Wednesday,
+            chrono::Thu => Thursday,
+            chrono::Fri => Friday,
+            chrono::Sat => Saturday
+        }
     }
 
 }
@@ -81,10 +90,10 @@ pub fn validate(spec: LeagueSpec) {
         spec.start_date.month as u32,
         spec.start_date.day as u32);
 
-    let good_start_date = spec.day_and_times.iter()
-        .any(|day_and_time| day_and_time.day.to_chrono_weekday() == start_date.weekday());
-    let good_end_date = spec.day_and_times.iter()
-        .any(|day_and_time| day_and_time.day.to_chrono_weekday() == end_date.weekday());
+    let good_start_date = spec.game_weekdays.iter()
+        .any(|game_weekdays| game_weekdays.day.to_chrono_weekday() == start_date.weekday());
+    let good_end_date = spec.game_weekdays.iter()
+        .any(|game_weekdays| game_weekdays.day.to_chrono_weekday() == end_date.weekday());
 
     if !good_start_date {
         println!("The start date does not occur on a day of the week listed.");
@@ -92,4 +101,19 @@ pub fn validate(spec: LeagueSpec) {
     if !good_end_date {
         println!("The end date does not occur on a day of the week listed.");
     }
+
+    //Make sure all locations are used at least once
+    let mut used_locations: HashSet<String> = HashSet::new();
+    for game_weekday in spec.game_weekdays.iter() {
+        for game_time in game_weekday.game_times.iter() {
+            for location_id in game_time.location_ids.iter() {
+                used_locations.insert(location_id.clone());
+            }
+        }
+    }
+
+    if used_locations.ne(&spec.locations.iter().map(|x| x.id.clone()).collect()) {
+        println!("Locations used in game_weekdays are not equal to the list of locations");
+    }
+
 }
